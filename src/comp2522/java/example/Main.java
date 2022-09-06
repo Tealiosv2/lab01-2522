2036a84f4fb5dec20c29145ada52140fa0e97446
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2022-09-06 14:11:22 -0700

    changed hello world to the ocean is very cold

--- src/comp2522/java/example/Main.java
+++ src/comp2522/java/example/Main.java
@@ -2,6 +2,6 @@ package example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The ocean is cold");
+        System.out.println("The ocean is very cold");
     }
 }
